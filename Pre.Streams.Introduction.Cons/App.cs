4bddb9efd1340b6a56672246d0b899795c392e88
Dieter Mourisse <dieter.mourisse@howest.e>
2023-05-02 10:45:46 +0200

    Add project files.

---
+++ Pre.Streams.Introduction.Cons/App.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pre.Streams.Introduction.Cons
+{
+    internal class App
+    {
+
+        public void ReadFile(string fileLocation)
+        {
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(fileLocation, Encoding.Default, true))
+                {
+                    Console.WriteLine(streamReader.ReadToEnd());
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file was not found");
+            }
+            
+        }
+
+        public bool WriteFile(string fileLocation, string content)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileLocation, true))
+                {
+                    writer.Write(content);
+                    return true;
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("De map bestaat niet op deze computer");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine($"Het bestand {fileLocation} kan niet weggeschreven worden.\n");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Er is een fout opgetreden");
+            }
+
+            return false;
+        }
+        public void Run()
+        {
+            ReadFile(@"c:\Howest\bestand.txt");
+            WriteFile(@"c:\Howest\bestand.txt", "ghi");
+        }
+    }
+}
---
+++ Pre.Streams.Introduction/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pre.Streams.Introduction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = @"c:\Howest";
+            
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string pad = openFileDialog.FileName;
+                lblFilename.Content = pad;
+            }
+        }
+    }
+}
